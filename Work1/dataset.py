26b633e7ef71ac8a92ba7a477241ce70d086cd5e
Victor <victor.badenas@gmail.com>
2020-10-07 11:32:08 +0200

    added docstrings and getters

--- Work1/dataset.py
+++ Work1/dataset.py
@@ -7,8 +7,23 @@ from utils import bytesToString
 
 
 class ArffFile:
+    """
+    Class responsible of loading and formatting the arff file data
+
+    input:
+        args:
+            arffPath: Path object with information on the path to arffFile
+
+    attributes:
+        path: path to arff file
+        data: pd.DataFrame with arff data in float values (normalised in range [0, 1])
+        metaData: arff metadata information
+        maps: dictionary with mapping information from string to int
+
+    """
     def __init__(self, arffPath):
         super(ArffFile, self).__init__()
+        self.path = arffPath
         data, self.metaData = loadarff(arffPath)
         self.maps = {}
         self.formatDataFrame(data)
@@ -33,7 +48,7 @@ class ArffFile:
     def convertStringsToInt(self, column, column_data):
         columnMap = self.createMap(column, column_data)
         self.maps[column] = columnMap
-        column_data = column_data.apply(lambda x: columnMap[x] if x!= '?' else x)
+        column_data = column_data.apply(lambda x: columnMap[x]/columnMap['max_range'] if x!= '?' else x)
         column_data[column_data == '?'] = np.mean(column_data[column_data != '?'])
         return column_data
 
@@ -46,8 +61,21 @@ class ArffFile:
         if '?' in columnData:
             columnData.remove('?')
         columnData = sorted(columnData)
-        return dict(zip(columnData, np.linspace(0, 1, len(columnData))))
+        max_range = len(columnData) - 1
+        int_labels = list(range(len(columnData)))
+        map_ = dict(zip(columnData, int_labels))
+        map_["max_range"] = max_range
+        return map_
+
+    def getData(self):
+        return self.data
+
+    def getMetaData(self):
+        return self.metaData
+
+    def getStringMapData(self):
+        return self.maps
 
 if __name__ == "__main__":
     arffFile = ArffFile(Path("./datasets/adult.arff"))
-    pass
+    print(arffFile.getData().head())
