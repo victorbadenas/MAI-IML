03d947fb8036fb3b19abf8c98cf51b36e5d9e9c7
Victor <victor.badenas@gmail.com>
2020-10-15 16:26:56 +0200

    fine tuned eps in DBSCAN

--- Work1/main.py
+++ Work1/main.py
@@ -1,10 +1,12 @@
 import argparse
 import warnings
+import numpy as np
 warnings.simplefilter(action='ignore')
 from dataset import ArffFile
 from pathlib import Path
 from sklearn.cluster import DBSCAN
 from clustering import KMeans, BisectingKMeans, KMeansPP
+from collections import Counter
 
 def parseArguments():
     parser = argparse.ArgumentParser()
@@ -20,12 +22,14 @@ class Main:
         print(self.args.arffFilesPaths)
         arffFile = ArffFile(self.args.arffFilesPaths[0])
         unsupervisedFeatures = arffFile.getData().copy().drop('class', axis=1)
+        y = arffFile.getData()['class']
+        print("number of classes=", len(np.unique(y)))
         print(unsupervisedFeatures.head())
-        arffFile.scatterPlot(figsize=(10, 10), ignoreLabel=True)
-        clustering = DBSCAN(n_jobs=-1)
+        # arffFile.scatterPlot(figsize=(10, 10), ignoreLabel=True)
+        clustering = DBSCAN(n_jobs=-1, eps=.75)
         labels = clustering.fit_predict(unsupervisedFeatures)
-        print([label for label in labels])
-        return
+        print(np.unique(labels))
+        print(Counter(labels))
 
 if __name__ == "__main__":
     args = parseArguments()
