93ba7924100820887f4fa92cf64413a4dcbb867d
Victor <victor.badenas@gmail.com>
2020-10-15 15:16:36 +0200

    added clustering imports

--- Work1/main.py
+++ Work1/main.py
@@ -4,7 +4,7 @@ warnings.simplefilter(action='ignore')
 from dataset import ArffFile
 from pathlib import Path
 from sklearn.cluster import DBSCAN
-
+from clustering import KMeans, BisectingKMeans, KMeansPP
 
 def parseArguments():
     parser = argparse.ArgumentParser()
@@ -18,12 +18,13 @@ class Main:
 
     def __call__(self):
         print(self.args.arffFilesPaths)
-        arffFile = ArffFile(self.args.arffFilesPaths[1])
-        print(arffFile.getData())
-        # arffFile.scatterPlot(figsize=(10, 10))
+        arffFile = ArffFile(self.args.arffFilesPaths[0])
+        unsupervisedFeatures = arffFile.getData().copy().drop('class', axis=1)
+        print(unsupervisedFeatures.head())
+        arffFile.scatterPlot(figsize=(10, 10), ignoreLabel=True)
         clustering = DBSCAN(n_jobs=-1)
-        labels = clustering.fit_predict(arffFile.getData())
-        print(labels)
+        labels = clustering.fit_predict(unsupervisedFeatures)
+        print([label for label in labels])
         return
 
 if __name__ == "__main__":
