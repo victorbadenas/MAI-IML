86ebfbbcc7f81aba8fe89adb29f49507b24bf81f
Victor <victor.badenas@gmail.com>
2020-10-15 15:20:40 +0200

    added page with documentation

--- Work1/clustering/bisectingKmeans.py
+++ Work1/clustering/bisectingKmeans.py
@@ -1,5 +1,9 @@
 import numpy as np
 from .kmeans import KMeans
+"""
+https://medium.com/@afrizalfir/bisecting-kmeans-clustering-5bc17603b8a2
 
+there is a python implementation there, but we shouldn't use it
+"""
 class BisectingKMeans(KMeans):
 	pass
