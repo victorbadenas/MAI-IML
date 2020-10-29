dd5d8c519193a1dfa253e24248d19f3b250c4f10
Victor <victor.badenas@gmail.com>
2020-10-29 18:56:49 +0100

    update

--- Work1/databaseTesting.py
+++ Work1/databaseTesting.py
@@ -27,23 +27,17 @@ pca = PCA(n_components=3)
 
 pcaData = pca.fit_transform(dataNumpy)
 
+# 3D PLOT IDEA
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+# plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 for label in set(clusters):
     subData = pcaData[clusters == label]
-    plt.scatter(subData[:,0], subData[:,1])
+    ax.scatter(subData[:, 0], subData[:, 1], subData[:, 2], s=60, label=label)
+    ax.view_init(30, 185)
+plt.legend()
 plt.show()
-
-# # 3D PLOT IDEA
-# from mpl_toolkits.mplot3d import Axes3D
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import pandas as pd
- 
-# # Dataset
-# df=pd.DataFrame({'X': range(1,101), 'Y': np.random.randn(100)*15+range(1,101), 'Z': (np.random.randn(100)*15+range(1,101))*2 })
- 
-# # plot
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(df['X'], df['Y'], df['Z'], c='skyblue', s=60)
-# ax.view_init(30, 185)
-# plt.show()
