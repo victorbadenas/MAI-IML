7abed57736d3093dd85a7df9327d662c9972fb2c
Victor <victor.badenas@gmail.com>
2020-10-29 19:50:10 +0100

    first metric ideas

--- Work1/databaseTesting.py
+++ Work1/databaseTesting.py
@@ -3,10 +3,21 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import DBSCAN
 from src.dataset import ArffFile
 import matplotlib.pyplot as plt
+from sklearn.metrics import cluster as clusteringMetrics
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+from pprint import pprint
 
 '''
 https://medium.com/@rodrigodutcosky/creating-a-3d-scatter-plot-from-your-clustered-data-with-plotly-843c20b78799
 '''
+
+def purity_score(y_true, y_pred):
+    contingency_matrix = clusteringMetrics.contingency_matrix(y_true, y_pred)
+    # return purity
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
 def runDBSCAN(data):
     clustering = DBSCAN(n_jobs=-1, eps=.75)
     labels = clustering.fit_predict(data)
@@ -27,10 +38,20 @@ pca = PCA(n_components=3)
 
 pcaData = pca.fit_transform(dataNumpy)
 
-# 3D PLOT IDEA
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-import numpy as np
+metrics = {}
+
+metrics["davies_bouldin_score"] = clusteringMetrics.davies_bouldin_score(unsupervisedFeatures, clusters)
+metrics["adjusted_mutual_info_score"] = clusteringMetrics.adjusted_mutual_info_score(y, clusters)
+metrics["adjusted_rand_score"] = clusteringMetrics.adjusted_rand_score(y, clusters)
+metrics["completeness_score"] = clusteringMetrics.completeness_score(y, clusters)
+metrics["fowlkes_mallows_score"] = clusteringMetrics.fowlkes_mallows_score(y, clusters)
+metrics["homogeneity_score"] = clusteringMetrics.homogeneity_score(y, clusters)
+metrics["mutual_info_score"] = clusteringMetrics.mutual_info_score(y, clusters)
+metrics["normalized_mutual_info_score"] = clusteringMetrics.normalized_mutual_info_score(y, clusters)
+metrics["v_measure_score"] = clusteringMetrics.v_measure_score(y, clusters)
+metrics["purity_score"] = purity_score(y, clusters)
+
+pprint(metrics)
 
 # plot
 fig = plt.figure()
