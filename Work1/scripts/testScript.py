7f478fcfa141bce24861a882937c32caed20f17b
Victor <victor.badenas@gmail.com>
2020-10-31 20:06:37 +0100

    renamed and restructured test script

---
+++ Work1/scripts/testScript.py
@@ -0,0 +1,100 @@
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+from sklearn.metrics import cluster as clusteringMetrics
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+from pprint import pprint
+from sklearn.cluster import KMeans as SKMeans
+import pandas as pd
+import sys
+from pathlib import Path
+
+sys.path.append("..")
+from src.clustering import KMeans, BisectingKMeans, KMeansPP, FCM
+from src.dataset import ArffFile
+
+def purity_score(y_true, y_pred):
+    contingency_matrix = clusteringMetrics.contingency_matrix(y_true, y_pred)
+    # return purity
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
+def runDBSCAN(data):
+    clustering = DBSCAN(n_jobs=-1, eps=.75)
+    labels = clustering.fit_predict(data)
+    return labels
+
+def sklearnKMeans(data, numOfClusters):
+    clustering = SKMeans(n_clusters=numOfClusters, init='random')
+    labels = clustering.fit_predict(data)
+    return labels
+
+def ourKMeans(data, numOfClusters):
+    clustering = KMeans(n_clusters=numOfClusters, verbose=False)
+    labels = clustering.fitPredict(data)
+    return labels
+
+def kMeansPP(data, numOfClusters):
+    clustering = KMeans(n_clusters=numOfClusters, init='k-means++', verbose=False)
+    labels = clustering.fitPredict(data)
+    return labels
+
+def bisectingKmeans(data, numOfClusters):
+    clustering = BisectingKMeans(n_clusters=numOfClusters, init='random', verbose=False)
+    labels = clustering.fitPredict(data)
+    return labels
+
+
+file_paths=[Path("../datasets/vote.arff"),Path("../datasets/adult.arff"),Path("../datasets/waveform.arff")]
+RESULTS_BASE = Path("../results")
+clusterMethods = [sklearnKMeans, ourKMeans, kMeansPP]
+K=10
+pca = PCA(n_components=3)
+
+
+for filePath in file_paths:
+    arffFile = ArffFile(filePath)
+    unsupervisedFeatures = arffFile.getData().copy()
+
+    labelColumn = unsupervisedFeatures.columns[-1]
+    unsupervisedFeatures = unsupervisedFeatures.drop(labelColumn, axis=1)
+    y = arffFile.getData()[labelColumn]
+
+    dataNumpy = unsupervisedFeatures.to_numpy()
+
+    for cluster_method in clusterMethods:
+        print(f"{cluster_method.__name__} is being computed:")
+
+        resultsDataframe = pd.DataFrame(index=["davies_bouldin_score", "adjusted_rand_score", "completeness_score", "purity_score"])
+        results_dir = RESULTS_BASE / filePath.stem / cluster_method.__name__
+        results_dir.mkdir(exist_ok=True, parents=True)
+        dfPath = results_dir / "data" / "metric.csv"
+        dfPath.parent.mkdir(exist_ok=True)
+
+        for k in range(2, K):
+            print(f"{k} number of clusters")
+            clusters = cluster_method(unsupervisedFeatures,k)
+            pcaData = pca.fit_transform(dataNumpy)
+            
+            metrics = dict()
+            metrics["davies_bouldin_score"] = clusteringMetrics.davies_bouldin_score(unsupervisedFeatures, clusters)
+            metrics["adjusted_rand_score"] = clusteringMetrics.adjusted_rand_score(y, clusters)
+            metrics["completeness_score"] = clusteringMetrics.completeness_score(y, clusters)
+            metrics["purity_score"] = purity_score(y, clusters)
+
+            kdf = pd.DataFrame.from_dict(metrics, orient='index', columns=[k])
+            resultsDataframe = resultsDataframe.join(kdf)
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            for label in set(clusters):
+                subData = pcaData[clusters == label]
+                ax.scatter(subData[:, 0], subData[:, 1], subData[:, 2], s=10, label=label)
+                ax.view_init(30, 185)
+            plt.legend()
+            plt.savefig(results_dir / f"3DScatter_{k}clusters.png")
+
+        resultsDataframe.to_csv(dfPath)
+
