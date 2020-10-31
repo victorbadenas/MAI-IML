ec54387283b8788e0835134168caf7b0b259d6e6
Victor <victor.badenas@gmail.com>
2020-10-31 17:41:10 +0100

    added clusteringMappingMetric function

--- Work1/metrics/clusterMappingMetric.py
+++ Work1/metrics/clusterMappingMetric.py
@@ -1,11 +1,20 @@
 import numpy as np
+from collections import Counter
+from sklearn.metrics import confusion_matrix
 
-class clusteringMappingMetric:
-    def __init__(self):
-        pass
-
-    def __call__(self, X, labels_pred, labels_gs):
-        """
-        docstring
-        """
-        pass
+def clusteringMappingMetric(self, X, labels_pred, labels_gs):
+    """
+    docstring
+    """
+    assert len(labels_gs) == len(labels_pred) == X.shape[0]
+    
+    X = np.array(X)
+    labels_pred = np.array(labels_pred)
+    labels_gs = np.array(labels_gs)
+    modified_labels = labels_pred.copy()
+    for clusterIdx in set(labels_pred):
+        clusterData = X[labels_pred == clusterIdx]
+        clusterDataGs = labels_gs[labels_pred == clusterIdx]
+        clusterAssignation = Counter(clusterDataGs).most_common(1)[0][0]
+        modified_labels[labels_pred == clusterIdx] = clusterAssignation
+    return confusion_matrix(labels_gs, modified_labels)
