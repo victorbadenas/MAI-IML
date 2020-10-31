4d41015e34b8ca9c3e8d6716d2fbb4809c9a2ce1
Victor <victor.badenas@gmail.com>
2020-10-31 16:38:34 +0100

    init clustering mapping metric

---
+++ Work1/metrics/clusterMappingMetric.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class clusteringMappingMetric:
+    def __init__(self):
+        pass
+
+    def __call__(self, X, labels_pred, labels_gs):
+        """
+        docstring
+        """
+        pass
