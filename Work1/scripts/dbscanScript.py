a8b37b9dc74604a467ff51b9732f9032800955c2
rmarrugat <rmarrugat@verbio.com>
2020-11-01 19:33:42 +0100

    Updating DBScan script.

--- Work1/scripts/dbscanScript.py
+++ Work1/scripts/dbscanScript.py
@@ -3,78 +3,55 @@ import numpy as np
 from pathlib import Path
 from sklearn.cluster import DBSCAN
 from matplotlib import pyplot as plt
-from sklearn.metrics import cluster as clusteringMetrics
 from sklearn.metrics import confusion_matrix
-from sklearn.decomposition import PCA
+from sklearn.neighbors import NearestNeighbors
 
-sys.path.append("..")
+sys.path.append(".")
 from src.dataset import ArffFile
-from metrics import clusteringMappingMetric
 
 def runDBSCAN(data, **kwargs):
     clustering = DBSCAN(**kwargs)
     return clustering.fit_predict(data)
 
 
-def main():
-    filePaths = [Path("../datasets/vote.arff"), Path("../datasets/adult.arff"), Path("../datasets/pen-based.arff")]
-
-    filePath = filePaths[1]
-
-    arffFile = ArffFile(filePath)
-    unsupervisedFeatures = arffFile.getData().copy()
-
-    labelColumn = unsupervisedFeatures.columns[-1]
-    unsupervisedFeatures = unsupervisedFeatures.drop(labelColumn, axis=1)
-    trueLabels = arffFile.getData()[labelColumn]
-
-    # dataNumpy = unsupervisedFeatures.to_numpy()
-    eps = .5
-    min_samples = range(10, 200, 10)
+def evaluate(data, y, eps, min_samples):
     n_jobs = -1
 
     for value in min_samples:
-        print(f"Evaluating DBSCAN with parameter={value}")
-        labels = runDBSCAN(unsupervisedFeatures, eps=eps, min_samples=value, n_jobs=n_jobs)
-        extendedConfusion = confusion_matrix(trueLabels, labels)
-        csvFilePath = Path(f"tmp/{filePath.stem}/")
-        csvFilePath.mkdir(exist_ok=True, parents=True)
-        np.savetxt(csvFilePath / f"{eps}eps_{value}min_samples_confusion.csv", extendedConfusion, delimiter=",", fmt='%i')
+        for ep in eps:
+            print(f"Evaluating DBSCAN with parameter={value}")
+            labels = runDBSCAN(data, eps=ep, min_samples=value, n_jobs=n_jobs)
+            extendedConfusion = confusion_matrix(y, labels)
+            np.savetxt(f"results/adult/dbscan/{ep}eps_{value}min_samples_confusion.csv", extendedConfusion, delimiter=",", fmt='%i')
 
 
-def findEps():
-    from sklearn.neighbors import NearestNeighbors
-    import seaborn as sns
-    sns.set()
-
-    filePaths = [Path("../datasets/vote.arff"), Path("../datasets/adult.arff"), Path("../datasets/pen-based.arff")]
-
-    filePath = filePaths[2]
-
-    arffFile = ArffFile(filePath)
-    unsupervisedFeatures = arffFile.getData().copy()
-
-    labelColumn = unsupervisedFeatures.columns[-1]
-    unsupervisedFeatures = unsupervisedFeatures.drop(labelColumn, axis=1)
-    _ = arffFile.getData()[labelColumn]
-
-    X = unsupervisedFeatures.to_numpy()
-
-    nn = 2
-    neigh = NearestNeighbors(n_neighbors=nn)
-    nbrs = neigh.fit(X)
-    distances, _ = nbrs.kneighbors(X)
-
-    distances = np.sort(distances, axis=0)
-    distances = distances[:,1]
-
-    plt.plot(distances)
+def findEps(data, Ks=[2]):
+    X = data.to_numpy()
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(Ks))]
+    for K, col in zip(Ks, colors):
+        neigh = NearestNeighbors(n_neighbors=K+1)
+        distances, _ = neigh.fit(X).kneighbors(X)
+        distances = np.sort(distances[:, -1], axis=0)
+        plt.plot(distances, color=tuple(col), label=f'{K} Nearest Neighbor')
     plt.tight_layout()
-    plotPath = Path(f"../results/{filePath.stem}/dbscan")
-    plotPath.mkdir(exist_ok=True, parents=True)
-    plt.savefig(plotPath / f"dbscan_{nn}.png")
+    plt.xlabel('Points sorted according to distance of Kth Nearest Neighbor')
+    plt.ylabel('Kth Nearest Neighbor Distance')
+    plt.title("K-dist plot for 'adult' dataset")
+    plt.legend()
+    plt.savefig(f"results/adult/dbscan/dbscanDistanceToKthNeighbor.png")
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    arffFile = ArffFile(Path("./datasets/adult.arff"))
+    data = arffFile.getData().copy()
+
+    labelColumn = data.columns[-1]
+    y = data[labelColumn]
+    data = data.drop(labelColumn, axis=1)
+
+    minPts = [10, 20, 30, 40, 50]
+    eps = [.55, .6, .65, .70, .75]
+
+    findEps(data, Ks=minPts)
+    #evaluate(data, y, eps, minPts)
