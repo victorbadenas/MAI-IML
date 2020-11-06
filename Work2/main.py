4d7ca58deebfc2b7155ea34665f3e033fa3cbcc8
Victor <victor.badenas@gmail.com>
2020-11-06 14:39:06 +0100

    added part1

--- Work2/main.py
+++ Work2/main.py
@@ -2,10 +2,15 @@ import json
 import argparse
 import warnings
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
 
 warnings.simplefilter(action='ignore')
 from src.utils import timer
 from src.dataset import ArffFile
+from src.visualize import Visualizer
+from src.pca import PCA
+
 
 def parseArguments():
     parser = argparse.ArgumentParser()
@@ -19,7 +24,7 @@ class Main:
             self.config = json.load(f)
 
     def __call__(self):
-        data, trueLabels = self.loadArffFile(Path(self.config["path"]))
+        self.step1()
 
     @staticmethod
     def loadArffFile(arffFilePath):
@@ -33,6 +38,34 @@ class Main:
         y = arffFile.getData()[labelColumn]
 
         return unsupervisedFeatures, y
+
+    def step1(self):
+        step1ResultsFolder = Path(self.config["resultsDir"]) / "step1"
+        step1ResultsFolder.mkdir(exist_ok=True, parents=True)
+
+        #step 1
+        data, trueLabels = self.loadArffFile(Path(self.config["path"]))
+        data = data.to_numpy()
+
+        #step 2 plot the original dataset (picked 3 dims with highest variance)
+        stds = list(zip(range(data.shape[1]), data.std(axis=0)))
+        stds.sort(key=lambda x: x[1])
+        dims = [i for i, _ in stds[:3]]
+        Visualizer.labeledScatter3D(data[:, dims], trueLabels, path=step1ResultsFolder / f"originalScatter.png")
+
+        #step 3
+        dataMean = np.mean(data, axis=0)
+
+        #step 4, 5, 6, 7
+        pca = PCA(n_components=3, print_=True)
+        reducedData = pca.fit_transform(data)
+
+        #step 8
+        Visualizer.labeledScatter3D(reducedData, trueLabels, path=step1ResultsFolder / f"pcaScatter.png")
+
+        #step 9
+        reconstructedData = pca.inverse_transform(reducedData)
+        Visualizer.labeledScatter3D(reconstructedData[:, dims], trueLabels, path=step1ResultsFolder / f"reconstructedScatter.png")
 
 if __name__ == "__main__":
     args = parseArguments()
