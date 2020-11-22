6d4e4547b38496a777d560bac70e1f44708adad8
Victor Badenas <victor.badenas@gmail.com>
2020-11-22 20:57:22 +0100

    added fit method

--- Work3/src/knn.py
+++ Work3/src/knn.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.spatial.distance import cdist
+from utils import convertToNumpy
+
 
 # distance metrics
 COSINE = 'cosine'
@@ -24,18 +26,28 @@ class KNN:
                  metric='minkowski',
                  mink_r=1, voting=''):
 
+        self.k = n_neighbors
         self.voting = voting
         self.weights = weights
         self.metric = metric
 
-    def fit(self, X):
-        raise NotImplementedError
+    def fit(self, X, y):
+        return self._fit(X, y)
 
     def predict(self, X):
-        raise NotImplementedError
+        return self._predict(X)
 
-    def fit_predict(self, X):
-        return self.fit(X).predict(X)
+    def fit_predict(self, X, y):
+        return self.fit(X, y).predict(X)
+
+    def _fit(self, X, y):
+        assert X.shape[0] >= self.k, f"Need a minimum of {self.k} points"
+        self.trainX = convertToNumpy(X.copy())
+        self.trainLabels = convertToNumpy(y.copy())
+        return self
+
+    def _predict(self, X):
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
